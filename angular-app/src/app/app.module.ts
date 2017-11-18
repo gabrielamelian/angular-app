fb956fc8e33d258d76d83c5f6ed4032d5e23945e
gabrielamelian <gabrielamelian@gmail.com>
2017-11-18 16:40:42 +1300

    Adds structure for basic components

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ImageComponent } from './form/image/image.component';
+import { DestinationComponent } from './form/destination/destination.component';
+import { FilterComponent } from './form/filter/filter.component';
+import { PreviewComponent } from './form/preview/preview.component';
+import { PreferencesComponent } from './form/preferences/preferences.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ImageComponent,
+    DestinationComponent,
+    FilterComponent,
+    PreviewComponent,
+    PreferencesComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-app/src/app/form/preferences/preferences.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preferences',
+  templateUrl: './preferences.component.html',
+  styleUrls: ['./preferences.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PreferencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
